1f6d4d3ed0abcd22c1d50d41816292af7981b894
Students <students@SAITMs-iMac-6.local>
2024-04-29 13:55:11 +0530

    todo list project

---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by Students on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TodoListView()
+        }
+    }
+}
